1bc11b0244ff76b5b414118b5b53cd65d38ec2de
Adore Ashish <adoreashish@gmail.com>
2016-05-21 17:09:33 +0530

    acl: Hibernate

---
+++ src/com/ashish/acl/HibernateTest.java
@@ -0,0 +1,20 @@
+package com.ashish.acl;
+
+/*public class HibernateTest {
+
+	public static void main(String[] args) {
+
+		UserDetails user1 = new UserDetails();
+		user1.setUserName("Ashish Gupta");
+		
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+		session.save(user1);
+		session.getTransaction().commit();
+		session.close();
+		
+	}
+
+}
+*/
